a56be9617832f7615e06561bbe27d129539a222c
chrisdiprima <chris.diprima121@gmail.com>
2025-03-26 20:30:40 -0400

    fixed bitwise calculator

--- tab2.js
+++ tab2.js
@@ -1,48 +1,98 @@
+let format = /[^01]/g;
+let system = "Binary";
+
 document.getElementById("num1").addEventListener("input", function () {
-	this.value = this.value.replace(/[^01]/g, "");
+	this.value = this.value.replace(format, "");
 	calculate();
 });
 
 document.getElementById("num2").addEventListener("input", function () {
-	this.value = this.value.replace(/[^01]/g, "");
+	this.value = this.value.replace(format, "");
 	calculate();
 });
 
 document.getElementById("operation").addEventListener("change", function () {
-	if (document.getElementById("operation").value === "NOT") {
-		document.getElementById("num2").style.display = "none";
+	calculate();
+});
+
+document.getElementById("format").addEventListener("change", function () {
+	if (this.value == "Decimal") {
+		format = /[^0-9]/g;
+		system = this.value;
+	} else if (this.value == "Binary") {
+		format = /[^01]/g;
+		system = this.value;
 	} else {
-		document.getElementById("num2").style.display = "block";
+		format = /[^0-9A-Fa-f]/g;
+		system = this.value;
 	}
 	calculate();
 });
 
 function calculate() {
-	let num1 = parseInt(document.getElementById("num1").value, 2) || 0;
-	let num2 = parseInt(document.getElementById("num2").value, 2) || 0;
+	if (system === "Binary") {
+		let num1 = parseInt(document.getElementById("num1").value, 2) || 0;
+		let num2 = parseInt(document.getElementById("num2").value, 2) || 0;
 
-	console.log(num1);
-	console.log(num2);
+		let operation = document.getElementById("operation").value;
+		let result;
 
-	let operation = document.getElementById("operation").value;
-	let result;
+		switch (operation) {
+			case "AND":
+				result = num1 & num2;
+				break;
+			case "OR":
+				result = num1 | num2;
+				break;
+			case "XOR":
+				result = num1 ^ num2;
+				break;
+		}
 
-	switch (operation) {
-		case "AND":
-			result = num1 & num2;
-			break;
-		case "OR":
-			result = num1 | num2;
-			break;
-		case "XOR":
-			result = num1 ^ num2;
-			break;
-		case "NOT":
-			result = ~num1;
-			break;
+		result = parseInt(result, 10).toString(2);
+
+		document.getElementById("bitwiseResult").textContent = result;
+	} else if (system === "Decimal") {
+		let num1 = parseInt(document.getElementById("num1").value, 10) || 0;
+		let num2 = parseInt(document.getElementById("num2").value, 10) || 0;
+
+		let operation = document.getElementById("operation").value;
+		let result;
+
+		switch (operation) {
+			case "AND":
+				result = num1 & num2;
+				break;
+			case "OR":
+				result = num1 | num2;
+				break;
+			case "XOR":
+				result = num1 ^ num2;
+				break;
+		}
+
+		document.getElementById("bitwiseResult").textContent = result;
+	} else {
+		let num1 = parseInt(document.getElementById("num1").value, 16) || 0;
+		let num2 = parseInt(document.getElementById("num2").value, 16) || 0;
+
+		let operation = document.getElementById("operation").value;
+		let result;
+
+		switch (operation) {
+			case "AND":
+				result = num1 & num2;
+				break;
+			case "OR":
+				result = num1 | num2;
+				break;
+			case "XOR":
+				result = num1 ^ num2;
+				break;
+		}
+
+		document.getElementById("bitwiseResult").textContent = result
+			.toString(16)
+			.toUpperCase();
 	}
-
-	result = parseInt(result, 10).toString(2);
-
-	document.getElementById("bitwiseResult").textContent = result;
 }
